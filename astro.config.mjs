c0c09eca0f8031322c10bbe59f9b7c4b848fee80
Mr-ant-cnmcw <3147814925@qq.com>
2026-07-23 12:46:00 +0800

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -63,7 +63,7 @@ const adapter = process.env.CF_WORKERS
 
 // https://astro.build/config
 export default defineConfig({
-	site:  "https://Annnnnt.github.io.github.io",
+	site:  "https://Annnnnt.github.io",
 
 	base: "/",
 	trailingSlash: "always",
